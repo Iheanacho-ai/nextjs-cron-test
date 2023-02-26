c23b6cb1391149cbb89a0a63fcb6b1691d427afb
Amarachi Iheanacho <amarachi2812@gmail.com>
2023-02-26 21:09:32 +0100

    first commit

---
+++ pages/api/text.ts
@@ -0,0 +1,29 @@
+import { NextApiResponse, NextApiRequest } from "next";
+const twillio = require('twilio');
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const myCellPhoneNumber = process.env.MY_NUMBER;
+const superSecretAPIKey = 'ABCD1234';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const client = twillio(accountSid, authToken);
+    const auth = req.headers.authorization;
+    if (auth === superSecretAPIKey) {
+        await client.messages
+            .create({
+                body: `\n
+						Create your todo list the night before with categories: 
+						\n- Work 
+						\n- Health 
+						\n- Relationships 
+						\n- Self Improvement 
+						\n 
+						\n Get better every day: https://jamesclear.com/continuous-improvement`,
+                from: '+12694754126', // my twilio testing number
+                to: `+1${myCellPhoneNumber}`
+            })
+            .then((message: String) => res.json(message), (err: Error) => res.json(err));
+    } else {
+        return res.status(401).end();
+    }
+}
